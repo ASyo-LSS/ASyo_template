c2ac3fe25d3cefb5bfb5393526f8059719f7a837
ASyo <3418959227@qq.com>
2023-09-23 20:49:09 +0800

    20230923

--- 过程设备/GCSB_OCT152023.py
+++ 过程设备/GCSB_OCT152023.py
@@ -2,14 +2,36 @@
 # Author：ASyo
 # Date：2023-9-12
 # Time：8:43
-from Printer_out_Close import Printer,Programe_colse
+from Printer_out_Close import Printer, Programe_colse
+
+'''
+参数说明
+Mpa：最高工作压力Pw
+T_max最高工作温度：30℃
+T_dmax：设计温度 根据工作温度加5-10℃
+
+V：容积
+Pd：设计压力
+Pc：计算设计压力=Pd
+Sm:设计寿命
+Whcd：危害程度 1.05-1.1倍
+Rl：容器类别
+Material：材料
+C1钢板厚度负偏差C18 ：C1=0.3mm
+Fai：焊接接头系数φ
+C2：腐蚀余量 每年按0.1mm/年腐蚀速率
+gczj：公称直径为1100mm、
+
+
+
+'''
 
 # 第一章Yi
 Mpa = float(input('请输入最高工作压力：'))
 V = float(input('请输入容积：'))
-Yi_1='1、空气缓冲\n'
-Yi_2='2、结构说明及管口表格\n'
-Yi = '第一章 整体设计\n'+Yi_1+Yi_2
+Yi_1 = '1、空气缓冲\n'
+Yi_2 = '2、结构说明及管口表格\n'
+Yi = '第一章 整体设计\n' + Yi_1 + Yi_2
 # 第二章Er
 Pd = 1.05 * Mpa
 Rl = input('请输入容器类别（1或2）：')
@@ -19,7 +41,8 @@ else:
     Rl = 'II'
 Er_6 = '第二章 技术参数设计（11个参数）\n1、介质：空气\n2、最高工作压力Pw：1.15Mpa\n3、最高工作温度：30℃\n4、设计寿命：20年\n5、容积：2.35m3\n6、设计压力Pd：一般取最高工作压力的1.05-1.1倍\n故Pd=1.05XPw=1.05X{0}=1.2075\n'.format(
     Pd)
-Er_7 = '7、设计温度Td：设计温度Td：根据工作温度加5-10℃，根据介质的危险程度确定工作温度的高低，如果介质危害长度低，取5℃反之取10℃\n故Td=30+5=35℃\n8、容器类别判定{0}（空气属于第二组介质）\n9、材料设计（确定主要受压元件材质）\n选Q345R\n10、钢板厚度负偏差C1：C1=0.3mm\n11、焊接接头系数φ：\n说明：双面焊、100%无损检测取φ=1.0\n12、腐蚀余量C2：根据设计寿命，每年按0.1mm/年负数速率为参考\nC2=20X0.1=2m'.format(Rl)
+Er_7 = '7、设计温度Td：设计温度Td：根据工作温度加5-10℃，根据介质的危险程度确定工作温度的高低，如果介质危害长度低，取5℃反之取10℃\n故Td=30+5=35℃\n8、容器类别判定{0}（空气属于第二组介质）\n9、材料设计（确定主要受压元件材质）\n选Q345R\n10、钢板厚度负偏差C1：C1=0.3mm\n11、焊接接头系数φ：\n说明：双面焊、100%无损检测取φ=1.0\n12、腐蚀余量C2：根据设计寿命，每年按0.1mm/年负数速率为参考\nC2=20X0.1=2m'.format(
+    Rl)
 Er = Er_6 + Er_7
 
 # 第三章 几何参数设计San
@@ -62,7 +85,7 @@ A = float(A_L[D])
 
 # 搜索封头容积
 V_R = float(V_L[D])
-San_1='1、确定承压设备公称直径:依据GB/T9019-2001\n初选本设计的承压设备公称直径为{0}mm\n'.format(gczj)
+San_1 = '1、确定承压设备公称直径:依据GB/T9019-2001\n初选本设计的承压设备公称直径为{0}mm\n'.format(gczj)
 
 San_21 = '（1）查公称直径为1100mm标准椭圆形封头容积\n得:V封头=0.1980，总深度H=300mm\n'.format(gczj, D, H, V)
 
@@ -90,13 +113,13 @@ if 2 < B < 3:
     San_jl = '结论：以上公称直径选取{0}mm是合格的\n'.format(gczj)
 else:
     San_jl = '结论：以上公称直径选取{0}mm是不合格的\n'.format(gczj)
-San_2=San_21+San_22+San_23+San_24
+San_2 = San_21 + San_22 + San_23 + San_24
 San = '第三章几何参数设计\n目标:解决筒体长度和公称直径的问题 DN\n判定原理：长径比为2-3\n'
 
 # 第四章强度设计
 
 Pc = Pd  # 计算压力
-Pcx=format(Pc,'.3f')
+Pcx = format(Pc, '.3f')
 Di = gczj  # 公称直径
 C1 = 0.3  # float(input('钢板厚度负偏差C1（在二、9）：'))
 C2 = 2  # float(input('请输入腐蚀余量C2（在二、9）：'))
@@ -106,7 +129,7 @@ han_jie_tou_xi_su = 1
 # 筒体计算厚度 TT_hd
 TT_hd = (Pc * Di) / (2 * xu_ying_li * han_jie_tou_xi_su - Pc)
 TT_hd1 = "%.3f" % TT_hd
-Si_11 = '1）先假设筒体厚度为6-16mm,查“常用钢板的许用应力”表得设计温度为60℃时的许用应力[σ]t=189MPa，将以上参数代入公式得筒体计算厚度为\nδ_c=({0}*{1})/2*{2}*{3}-{0}≈{4}\n'.format(
+Si_11 = '1）先假设筒体厚度为6-16mm,查“常用钢板的许用应力”表得设计温度为60℃时的许用应力[σ]t=189MPa，将以上参数代入公式得筒体计算厚度为\nδ_c=(p_c D_i)/(2[σ]_tφ-p_c )=({0}*{1})/2*{2}*{3}-{0}≈{4}\n'.format(
     Pcx, Di, xu_ying_li, han_jie_tou_xi_su, TT_hd1)
 # 求设计壁厚δ_d sjbh
 sjbh = TT_hd + C2
@@ -125,37 +148,39 @@ else:
 Si_13 = '3)求名义厚度δ_n\n确定名义壁厚需要考虑二个因素，一个是钢板的厚度负偏差，另外一个是钢板的标准厚度系列，C1={0}mm，{1}+{0}={4}mm（向上圆整到钢板的标准厚度系列）,故δ_n={3}mm（向上取偶数）\n'.format(
     C1, sjbh1, mhd, tygj, mhd2)
 Si_14 = '4）检查\nδ_n在假设筒体厚度范围为6-16mm范围，故最后得筒体的名义壁厚为{0}mm\n'.format(mhd)
-Si_1 = '1、筒体计算壁厚δ_c\n'+Si_11 + Si_13 + Si_14
+Si_1 = '1、筒体计算壁厚δ_c\n' + Si_11 + Si_13 + Si_14
 p_c = Pd
 D_i = gczj
 a_t = 189
 fai = 1
 # 封头计算厚度
 FTH = (p_c * D_i) / (2 * a_t - p_c)
-FTHx=format(FTH,'.3f')
-Si_21 = '1）先假设封头厚度为6-16mm，查“常用钢板的许用应力” (Q245R)表得设计温度为30°C时的许用应力[σ]t=189MPa，将以上参数代入公式得封头计算厚度为\n(p_c D_i)/2[σ]_tφ-p_c={0}\n'.format(FTHx)
+FTHx = format(FTH, '.3f')
+Si_21 = '1）先假设封头厚度为6-16mm，查“常用钢板的许用应力” (Q245R)表得设计温度为30°C时的许用应力[σ]t=189MPa，将以上参数代入公式得封头计算厚度为\n(p_c D_i)/2[σ]_tφ-p_c={0}\n'.format(
+    FTHx)
 # 求设计壁厚δd fai_d
-fai_d=TT_hd+C2
-fai_dx=format(fai_d,'.3f')
-Si_22 = '2）求设计壁厚δd\nδd=δc+C2={0}+{1}={2}mm\n'.format(TT_hd1,C2,fai_dx)
+fai_d = TT_hd + C2
+fai_dx = format(fai_d, '.3f')
+Si_22 = '2）求设计壁厚δd\nδd=δc+C2={0}+{1}={2}mm\n'.format(TT_hd1, C2, fai_dx)
 # 求名义壁厚δn fai_n
-fai_n=C1+fai_d
-fai_nx=format(fai_n,'3f')
-fai_n_int=int(fai_n)
-if fai_n_int%2==0:
-    fai_n_int+=2
+fai_n = C1 + fai_d
+fai_nx = format(fai_n, '3f')
+fai_n_int = int(fai_n)
+if fai_n_int % 2 == 0:
+    fai_n_int += 2
 else:
-    fai_n_int+=1
+    fai_n_int += 1
 
-Si_23 = '3）求名义壁厚δn\n确定名义壁厚需要考虑二个因素，一个是钢板的厚度负偏差，另外一个是钢板的标准厚度系列，C1={0}mm，{1}+{0}={2} mm（向上圆整到钢板的标准厚度系列）\n故δn={3}mm\n'.format(C1,fai_dx,fai_nx,fai_n_int)
-if 6<=fai_n_int<=16:
+Si_23 = '3）求名义壁厚δn\n确定名义壁厚需要考虑二个因素，一个是钢板的厚度负偏差，另外一个是钢板的标准厚度系列，C1={0}mm，{1}+{0}={2} mm（向上圆整到钢板的标准厚度系列）\n故δn={3}mm\n'.format(
+    C1, fai_dx, fai_nx, fai_n_int)
+if 6 <= fai_n_int <= 16:
     Si_24 = 'δn={}mm在假设封头厚度为6-16mm范围，有效\n'.format(fai_n_int)
 else:
     Si_24 = 'δn={}mm不在假设封头厚度为6-16mm范围，无效\n'.format(fai_n_int)
-Si_2 = '2、标准椭圆形封头计算壁厚δc\n'+Si_21 + Si_22 + Si_23 + Si_24
-Si = '第四章 强度设计（强度计算公式人工计算）\n目标：计算筒体和封头的壁厚，最终确定筒体和封头的名义壁厚\n'+Si_1 + Si_2
+Si_2 = '2、标准椭圆形封头计算壁厚δc\n' + Si_21 + Si_22 + Si_23 + Si_24
+Si = '第四章 强度设计（强度计算公式人工计算）\n目标：计算筒体和封头的壁厚，最终确定筒体和封头的名义壁厚\n' + Si_1 + Si_2
 
-All=Yi+Er+San+Si
+All = Yi + Er + San + Si
 print(All)
 Programe_colse()
 # Q345R和Q245R
